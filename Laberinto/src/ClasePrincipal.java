9b72d4cc398feab83207a477ec34a7e7f1a47ec2
USUARIO <USUARIO@DESKTOP-7NJHQ92>
2020-10-05 17:37:42 +0200

    método esComienzo, estaCompletado y celdaAleatoria

--- Laberinto/src/ClasePrincipal.java
+++ Laberinto/src/ClasePrincipal.java
@@ -8,12 +8,14 @@ public class ClasePrincipal {
 	
 	public static void construirLab() {
 		Laberinto laberinto = new Laberinto();
-		laberinto = introducirCeldas(laberinto, 3);
+		laberinto = introducirCeldas(laberinto, 3); //Tamaño del laberinto
+		laberinto = wilson(laberinto);
+		
 		mostrarCeldas(laberinto);
 		
 	}
 	
-	public static Laberinto introducirCeldas(Laberinto lab, int tamañoLab) { //introducir tamaño y celdas
+	public static Laberinto introducirCeldas(Laberinto lab, int tamañoLab) { //Metodo que inicializa el laberinto.
 		lab.setColumnas(tamañoLab);
 		lab.setFilas(tamañoLab);
 		int[] posicion;
@@ -28,12 +30,74 @@ public class ClasePrincipal {
 				c.setPosicion(posicion);
 				c.setVecinos(vecinos);
 				c.setValor(0);
+				c.setVisitado(false);
 				celdas[i][j] = c;
 			}
 		}
 		lab.setCeldas(celdas);
 		return lab;
 		
+	}
+	
+	public static Laberinto wilson(Laberinto lab) {
+		Celda nextC = new Celda();
+		
+		if(esComienzo(lab)) { // true si estan todas ls celdas NO visitadas
+			System.out.println("El laberinto se empezará a formar...");
+			
+			//AQUI LA MAGIA
+			
+		}else if (estaCompletado(lab)) { //True si no quedan celdas por visitar
+			System.out.println("El laberinto esta completo");
+			
+			//AQUI MAS MAGIA
+			
+		}
+		
+		return lab;
+	}
+	
+	public static Celda celdaAleatoria(Laberinto lab) {
+		Celda c = new Celda();
+		Celda[][] celdas = lab.getCeldas();
+		int fila, columna;
+		int[] posicion = new int[2];
+		while(true) {
+			fila = (int) Math.floor(Math.random()*lab.getFilas());
+			columna = (int) Math.floor(Math.random()*lab.getColumnas());
+			if(celdas[fila][columna].isVisitado() == false) {
+				posicion[0] = fila;
+				posicion[1] = columna;
+				c.setPosicion(posicion);
+				break;
+			}
+		}
+		return c;
+	}
+	
+	public static boolean estaCompletado(Laberinto lab) {
+		Celda[][] celdas = lab.getCeldas();
+		for(int i = 0; i < celdas.length; i++) {
+			for(int j = 0; j < celdas[0].length; j++) {
+				if (!celdas[i][j].isVisitado()) {
+					return false;
+				}
+			}
+		}
+		return true; 
+	}
+	
+	public static boolean esComienzo(Laberinto lab) {
+		Celda[][] celdas = lab.getCeldas();
+		for(int i = 0; i < celdas.length; i++) {
+			for(int j = 0; j < celdas[0].length; j++) {
+				if (celdas[i][j].isVisitado()) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
 	public static void mostrarCeldas(Laberinto lab) {
