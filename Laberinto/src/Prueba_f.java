1b964461f2c0967f5a5e239d42596bb2cab93739
Pedro <Pedro@DESKTOP-L8I1U6F>
2020-11-07 13:01:34 +0100

    Modificacacion y la Prueba de la frontera

---
+++ Laberinto/src/Prueba_f.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Prueba_f {
+
+	public static void main(String[] args) {
+		Frontera front = new Frontera();
+		Random r = new Random();
+		
+		
+	for (int i =0 ; i< 12 ;i++ ) {
+		int[] estado = {0,0};
+		estado[0]=r.nextInt(100);
+		estado[1]=r.nextInt(100);
+		Nodo n1 = new Nodo(i, estado, r.nextInt(100),r.nextInt(100), r.nextInt(100), null, r.nextInt(100));
+		front.insertarNodo(n1);
+	}
+		
+		Collections.sort(front.getFrontera());
+	
+		front.recorrerLista();
+
+	}
+
+}
